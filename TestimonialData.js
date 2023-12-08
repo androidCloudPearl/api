cba4f36ef95e8d6226a0485ae5166658c89bf52c
androidCloudPearl <145786942+androidCloudPearl@users.noreply.github.com>
2023-12-08 14:29:49 +0530

    Create TestimonialData.js

---
+++ TestimonialData.js
@@ -0,0 +1,34 @@
+
+{
+     
+    "testimonials": [
+      {
+        "id": 1,
+        "name": "Girish Mahajan",
+        "message": "A Heartfelt Invitation to Kanhaiya and the Entire Family",
+        "image": "C:/Users/USER/Projects/Amantran_App/src/images/UserProfile.png",
+        "video": { "uri": "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/BigBuckBunnyVideo.mp4" }
+      },
+      {
+        "id": 2,
+        "name": "Nandkishor Mahajan",
+        "message": "A Heartfelt Invitation to Kanhaiya and the Entire Family",
+        "image": "../../images/UserProfile.png",
+        "video": { "uri": "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/BigBuckBunnyVideo.mp4" }
+      },
+      {
+        "id": 3,
+        "name": "Raksha Khadse .MP",
+        "message": "A Heartfelt Invitation to Kanhaiya and the Entire Family",
+        "image": "../../images/UserProfile.png",
+        "video": { "uri": "https://commondatastorage.googleapis.com/gtv-videos-bucket/CastVideos/dash/BigBuckBunnyVideo.mp4" }
+      }
+    ],
+    "counters": {
+      "id": 1,
+      "Successful_events": 400,
+      "Upcoming_events": 167,
+      "submits_Invitation": 387
+    }
+  }
+  
